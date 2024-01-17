da21fe3832cd522a92dc54cdc5dfe8e810eb5086
elmae <dmilesdrsgs@gmail.com>
2024-01-17 13:59:52 -0400

    add: se modifica el navbar js para cambiar el bg

--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,10 +1,21 @@
 // Add your custom JS here.
-(new IntersectionObserver(function(e,o){   
-  if (e[0].intersectionRatio > 0){ 
-    document.documentElement.removeAttribute('class');   
-} else {
-    document.documentElement.setAttribute('class','stuck');};
-  })).observe(document.querySelector('.trigger'))
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.querySelector('.navbar');
+  var scrollPosition = window.scrollY;
+
+  // Añade un listener para el evento de scroll
+  window.addEventListener('scroll', function() {
+    // Actualiza la posición de desplazamiento
+    scrollPosition = window.scrollY;
+
+    // Verifica si el scroll es mayor a 10px y actualiza el fondo del navbar
+    if (scrollPosition > 10) {
+      navbar.style.backgroundColor = '#ffffff'; // Cambia a blanco (#ffffff)
+    } else {
+      navbar.style.backgroundColor = 'transparent'; // Vuelve al fondo transparente
+    }
+  });
+});
 
 
   
