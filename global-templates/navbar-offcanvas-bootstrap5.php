34231427fd576dd8ab54c96c1d1c36ad90d030f6
elmae <dmilesdrsgs@gmail.com>
2024-01-17 13:56:54 -0400

    change: se modifica el navbar y hero canvas

--- global-templates/navbar-offcanvas-bootstrap5.php
+++ global-templates/navbar-offcanvas-bootstrap5.php
@@ -12,43 +12,38 @@ defined( 'ABSPATH' ) || exit;
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
+<nav id="main-nav" class="navbar navbar-expand-lg sticky-top bg-*" aria-labelledby="main-nav-label">
 
-	<h2 id="main-nav-label" class="screen-reader-text">
-		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-	</h2>
+  <h2 id="main-nav-label" class="screen-reader-text">
+    <?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+  </h2>
 
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+  <div class="<?php echo esc_attr( $container ); ?>">
 
-		<!-- Your site branding in the menu -->
-		<?php get_template_part( 'global-templates/navbar-branding' ); ?>
+    <!-- Your site branding in the menu -->
+    <span class="d-none d-md-block">
+      <?php get_template_part( 'global-templates/navbar-branding' ); ?>
+    </span>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNavOffcanvas"
+      aria-controls="navbarNavOffcanvas" aria-expanded="false"
+      aria-label="<?php esc_attr_e( 'Open menu', 'understrap' ); ?>">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-		<button
-			class="navbar-toggler"
-			type="button"
-			data-bs-toggle="offcanvas"
-			data-bs-target="#navbarNavOffcanvas"
-			aria-controls="navbarNavOffcanvas"
-			aria-expanded="false"
-			aria-label="<?php esc_attr_e( 'Open menu', 'understrap' ); ?>"
-		>
-			<span class="navbar-toggler-icon"></span>
-		</button>
+    <div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="navbarNavOffcanvas">
 
-		<div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="navbarNavOffcanvas">
-
-			<div class="offcanvas-header justify-content-end">
-				<button
-					class="btn-close btn-close-white text-reset"
-					type="button"
-					data-bs-dismiss="offcanvas"
-					aria-label="<?php esc_attr_e( 'Close menu', 'understrap' ); ?>"
-				></button>
-			</div><!-- .offcancas-header -->
-
-			<!-- The WordPress Menu goes here -->
-			<?php
+      <div class="offcanvas-header justify-content-end">
+        <button class="btn-close btn-close-white text-reset" type="button" data-bs-dismiss="offcanvas"
+          aria-label="<?php esc_attr_e( 'Close menu', 'understrap' ); ?>"></button>
+      </div><!-- .offcancas-header -->
+      <!-- Navbar brand -->
+      <!-- Your site branding in the menu -->
+      <span class="text-center navbar-brand py-3 mt-2 mt-lg-0 d-md-none">
+        <?php get_template_part( 'global-templates/navbar-branding' ); ?>
+      </span>
+      <!-- The WordPress Menu goes here -->
+      <?php
 			wp_nav_menu(
 				array(
 					'theme_location'  => 'primary',
@@ -62,8 +57,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 				)
 			);
 			?>
-		</div><!-- .offcanvas -->
+    </div><!-- .offcanvas -->
 
-	</div><!-- .container(-fluid) -->
+  </div><!-- .container(-fluid) -->
 
 </nav><!-- #main-nav -->
